535ed02ace83cc179d86fc0a168766c674bd0fc9
Oladele Sowemimo <olsowemi@microsoft.com>
2021-08-28 20:33:14 -0500

    removed files

---
+++ httpTrigger/OnPaymentReceived.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace demofuncs
+{
+    public static class OnPaymentReceived
+    {
+        [FunctionName("OnPaymentReceived")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            [Queue("orders")] IAsyncCollector<Order> orderQueue,
+            ILogger log)
+        {
+            log.LogInformation("Received a payment");
+
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var order = JsonConvert.DeserializeObject<Order>(requestBody);
+            await orderQueue.AddAsync(order);
+            
+            log.LogInformation($"Order {order.OrderId} received from {order.Email} for Product{order.ProductId}");
+
+            return new OkObjectResult($"Thank you for the purchase");
+        }
+    }
+
+    public class Order
+    {
+        public string OrderId { get; set; }
+        public string ProductId { get; set; }
+        public string Email { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
